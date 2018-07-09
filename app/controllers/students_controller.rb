2f2ad29f9130759567f9d657f4c5ce72dad5c98e
Meredith Reece <mardebugger@gmail.com>
2018-07-09 00:19:59 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
   def activate
     @student = Student.find(params[:id])
     @student.update(active: true)
-    redirect 'student_path(@student)'
+    redirect_to 'student_path(@student)'
   end
 
   private
